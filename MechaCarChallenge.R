50a87fa9ca896ad6f7606daaf8c30ca9a4af348d
Tucker Fischer <tucker.j.fischer@gmail.com>
2021-06-19 21:51:32 -0500

    T-Tests

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -15,3 +15,16 @@ total_summary
 
 lot_summary <- Suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
 lot_summary
+
+total_summary
+lot_summary
+
+t.test((Suspension$PSI), mu=mean(1500))
+
+lot1 <- subset(Suspension, Manufacturing_Lot=="Lot1")
+lot2 <- subset(Suspension, Manufacturing_Lot=="Lot2")
+lot3 <- subset(Suspension, Manufacturing_Lot=="Lot3")
+
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot3$PSI,mu=1500)
